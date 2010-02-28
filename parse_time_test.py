dbb714a5bce88ed1673130964492b1cbe74712ac
Flávio Cruz <flaviocruz@gmail.com>
2010-02-28 16:37:21 +0000

    rename parse_csv_five_runs to parse_time_test, make it parse all kinds of time test output

--- parse_time_test.py
+++ parse_time_test.py
@@ -3,11 +3,11 @@
 import re
 import sys
 
-pattern = re.compile('test \'(.*)\': cputime average five runs is (.*) \(.*\)')
+pattern = re.compile('test \'(.*)\': cputime (.*) is (.*) \(.*\)')
 
 for line in sys.stdin:
   m = pattern.match(line)
   if m:
     test = m.group(1)
-    time = m.group(2)
+    time = m.group(3)
     print test + " ; " + time
