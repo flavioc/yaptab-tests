fb43abf99fa353546e46a330e47b5c8381c8b3fe
Flávio Cruz <flaviocruz@gmail.com>
2010-03-01 02:29:20 +0000

    another test

--- sub_tests/test_lrtc1_data_chain.pl
+++ sub_tests/test_lrtc1_data_chain.pl
@@ -1,5 +1,6 @@
 
 :- consult('driver'),
-   consult('anc_l.pl').
+   consult('anc_l.pl'),
+   consult('data/chain.pl').
 
 debug_query :- backtrack_and_print(anc_l(_,_)), fail.
---
+++ sub_tests/test_lrtc_down.pl
@@ -0,0 +1,11 @@
+
+:- consult('driver'),
+   consult('anc_l.pl'),
+   consult('data/cycles.pl').
+
+debug_query :-
+  backtrack_and_print(anc_l(_,_)),
+  backtrack_and_print(anc_l(X,X)),
+  backtrack_and_print(anc_l(f(F),f(F))),
+  backtrack_and_print(anc_l([L],[L])),
+  fail.
