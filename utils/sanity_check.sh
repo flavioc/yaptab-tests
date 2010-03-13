c30244552d837ee5e59735c6383fc604dec68582
Flávio Cruz <flaviocruz@gmail.com>
2010-03-13 01:42:05 +0000

    add load answers sanity check. use IGNORE_TABLES.

--- utils/sanity_check.sh
+++ utils/sanity_check.sh
@@ -2,6 +2,11 @@
 
 YAP=$1
 
+if [ -z "$YAP" ]; then
+  echo "usage: sanity_check.sh <YAP>"
+  exit 1
+fi
+
 run_test () {
   test=$1
   bash ./run_debug_test_suite.sh -o $test \
@@ -9,7 +14,22 @@ run_test () {
     $YAP || exit 1
 }
 
+run_load_test () {
+  test=$1
+  bash ./run_debug_test_suite.sh -o $test \
+    -c run_config_files/yap_subsumptive_load_answers.pl \
+    $YAP || exit 1
+}
+
 run_test basic_tests
 run_test path_small_tests
 run_test sub_tests
 run_test complex_types_tests
+
+export IGNORE_TABLES=yes
+run_load_test basic_tests
+run_load_test path_small_tests
+run_load_test sub_tests
+run_load_test complex_types_tests
+
+bash utils/clear_results.sh
