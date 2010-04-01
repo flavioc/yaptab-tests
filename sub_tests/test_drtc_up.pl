910aa57a63b649475a27df44a06d27f775325d7e
Flávio Cruz <flaviocruz@gmail.com>
2010-04-01 23:24:35 +0100

    remove use_subsumptive_tabling declaration

--- sub_tests/test_drtc_up.pl
+++ sub_tests/test_drtc_up.pl
@@ -2,8 +2,6 @@
    consult('anc_d.pl'),
    consult('data/cycles.pl').
 
- :- use_subsumptive_tabling anc_d/2.
-
 debug_query :- 
 	backtrack_and_print( anc_d([L],[L]) ),
 	backtrack_and_print( anc_d(f(F),f(F)) ),
