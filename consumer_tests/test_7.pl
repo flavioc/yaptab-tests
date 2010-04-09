1358c639e637c8d60ba94cee8a5bf1587415d5a0
Flávio Cruz <flaviocruz@gmail.com>
2010-04-10 00:22:58 +0100

    add test for internal subgoals

---
+++ consumer_tests/test_7.pl
@@ -0,0 +1,14 @@
+% tests internal general subgoal, without dependencies.
+
+:- table p/2.
+
+:- use_grounded_tabling p/2.
+
+e(1, 2).
+e(1, 1).
+p(5, 5).
+
+p(X, Y) :- p(A, B), e(X, Y).
+p(X, Y) :- e(X, Y).
+
+debug_query :- p(1, X), write(X), nl, fail.
