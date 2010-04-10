fb54b00904bfff248f0886aa1355702c74ef0b14
Flávio Cruz <flaviocruz@gmail.com>
2010-04-10 01:04:04 +0100

    add test 6: test internal general subgoals with outside dependencies

---
+++ consumer_tests/test_6.pl
@@ -0,0 +1,19 @@
+% tests internal general subgoals with outside dependencies.
+
+:- table t/1.
+:- table p/2.
+:- use_variant_tabling t/1.
+:- use_grounded_tabling p/2.
+
+t(X) :- p(1, X).
+t(1).
+t(2).
+
+p(1, X) :- t(X).
+p(X, Y) :- p(A, B), e(X, Y).
+
+e(1, 2).
+e(1, 3).
+e(5, 3).
+
+debug_query :- t(X), write(X), nl, fail.
