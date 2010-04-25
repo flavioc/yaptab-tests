b331390c7a6cb45b2f808d32207ddf4e115b2a24
Flávio Cruz <flaviocruz@gmail.com>
2010-04-25 19:18:43 +0100

    new test

---
+++ consumer_tests/test_47.pl
@@ -0,0 +1,13 @@
+% Initial generator and two consumers p(1, 3, C).
+
+:- table p/3.
+
+:- use_grounded_tabling p/3.
+
+p(1, 2, 3).
+p(1, 3, 2).
+p(1, 3, 9).
+p(1, 4, 5).
+p(5, 6, 7).
+
+debug_query :- p(1, A, B), p(1, 3, C), p(D, E, F), write((A, B, C, D, E, F)), nl, fail.
