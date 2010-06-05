1f352c3e630019ad7f775608e7970752cb789de7
Flávio Cruz <flaviocruz@gmail.com>
2010-06-05 19:55:16 +0100

    better parse to csv script

--- utils/parse_time_test.py
+++ utils/parse_time_test.py
@@ -3,11 +3,18 @@
 import re
 import sys
 
-pattern = re.compile('test \'(.*)\': cputime (.*) is ([0-9]*\.?[0-9]*)')
+#pattern = re.compile('test \'(.*)\': cputime (.*) is ([0-9]*\.?[0-9]*)')
+pattern = re.compile('test \'(.*)\': (.*)')
 
 for line in sys.stdin:
   m = pattern.match(line)
   if m:
     test = m.group(1)
-    time = m.group(3)
-    print test + " ; " + time
+    timestr = m.group(2)
+    pat2 = re.compile('cputime (.*) is ([0-9]*\.?[0-9]*)')
+    m2 = pat2.match(timestr)
+    if m2:
+      time = m2.group(2)
+      print test + " ; " + time
+    else:
+      print test + " ; "
